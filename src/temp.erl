289cee8ec28f3907ce9b7e815648c84116ac3548
Gaizka Rubio Landaluze <grubio@stratio.com>
2016-10-18 13:41:50 +0200

    Define for atoms

--- src/temp.erl
+++ src/temp.erl
@@ -12,10 +12,13 @@
 %% API
 -export([convert/1]).
 
-f2c(F) -> io:fwrite("~w", [{c,(F-32)/1.8}]).
+-define(F, f).
+-define(C, c).
 
-c2f(C) -> io:fwrite("~w", [{f,(C+32)*1.8}]).
+f2c(F) -> io:fwrite("~w", [{?C,(F-32)/1.8}]).
 
-convert({Type, Temp}) when Type =:= f -> f2c(Temp);
-convert({Type, Temp}) when Type =:= c -> c2f(Temp).
+c2f(C) -> io:fwrite("~w", [{?F,(C+32)*1.8}]).
+
+convert({Type, Temp}) when Type =:= ?F -> f2c(Temp);
+convert({Type, Temp}) when Type =:= ?C -> c2f(Temp).
 
