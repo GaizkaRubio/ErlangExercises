0fa51fa38eb819ac4b5b04631b8c9583787e3cb0
Gaizka Rubio Landaluze <grubio@stratio.com>
2016-10-19 09:52:28 +0200

    min max of a list exercise

--- src/lists1.erl
+++ src/lists1.erl
@@ -10,5 +10,18 @@
 -author("grubio").
 
 %% API
--export([]).
+-export([min_max/1]).
 
+min(L) -> min(lists:nth(1, L), L).
+
+min(N, [Head|Tail]) when Head =< N -> min(Head, Tail);
+min(N, [Head|Tail]) when Head >= N -> min(N, Tail);
+min(N, []) -> N.
+
+max(L) -> max(lists:nth(1, L), L).
+
+max(N, [Head|Tail]) when Head >= N -> max(Head, Tail);
+max(N, [Head|Tail]) when Head =< N -> max(N, Tail);
+max(N, []) -> N.
+
+min_max(L) -> {min(L), max(L)}.
